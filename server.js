2cbd51ea46ef632683d5e95a4dd365ef9697f55c
Adrian Diaz <adrian.diaz.poletti@daedalus.de>
2026-02-22 16:08:32 +0100

    added settings for prod

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = 5050;
+
+app.use(express.json());
 
 app.get("/api/test", (req, res) => {
-  res.json({ message: "Server is working!" });
+  res.json({ message: "API working" });
 });
 
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/build/index.html"));
+});
+
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
